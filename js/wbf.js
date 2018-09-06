2cc75fb6d0380643de959ecf8a96d6862dead925
mc17uulm <marco.combosch@uni-ulm.de>
2018-09-06 17:15:53 +0200

    first steps
    form
    wp backend
    ajax

---
+++ js/wbf.js
@@ -0,0 +1,108 @@
+jQuery(document).ready(($) => {
+
+    console.log("wbf initalized");
+
+    const check_btn = $('#check_btn');
+    let date = -1;
+    let err = "";
+
+    // initalize event listener for check_btn
+    check_btn.on('click', (e) => {
+
+        e.preventDefault();
+
+        console.log("clicked");
+
+        // bool if woman or not
+        const woman = $('input[name=salutation]:checked').attr('id') === "frau";
+
+        const firstname = $('#firstName').val();
+        if(firstname === "")
+        {
+            err += "<li>Bitte geben Sie Ihren Vornamen an</li>>";
+        }
+        const lastname = $('#lastName').val();
+        if(lastname === "")
+        {
+            err += "<li>Bitte geben Sie Ihren Nachnamen an</li>>";
+        }
+        const email = $('#email').val();
+        if(email === "")
+        {
+            err += "<li>Bitte geben Sie eine E-Mail-Adresse an</li>>";
+        }
+        if(!validateEmail(email))
+        {
+            err += "<li>Bitte geben Sie eine gültige E-Mail-Adresse an</li>";
+        }
+        const customer = $('input[name=customer]:checked').attr('id') === "y_customer";
+        if(date === -1)
+        {
+            err += "<li>Bitte geben Sie ein Datum an</li>";
+        }
+        const impressum = $('input[name=impressum]:checked').attr('id') === "y_impressum";
+        const dsgvo = $('input[name=dsgvo]:checked').attr('id');
+        const cms = $('input[name=cms]:checked').attr('id') === "y_cms";
+        const important = $('input:checkbox[name=important]:checked').map((e, a) => {return $(a).attr('id')}).get();
+
+        if(important.length < 1)
+        {
+            err += "<li>Bitte geben Sie mindestens einen wichtigen Grund für Ihr Webprojekt an</li>";
+        }
+        if(err.length > 1)
+        {
+            $('#msg_h').text("Fehler");
+            $('#msg_p').html(`Bitte korrigieren Sie folgende Fehler:<br><ul>${err}</ul>`);
+            err = "";
+            $('#msg').removeClass();
+            $('#msg').addClass('alert alert-danger');
+            $('#msg').show();
+            return;
+        }
+        const obj = {
+            woman: woman,
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            customer: customer,
+            date: date,
+            impressum: impressum,
+            dsgvo: dsgvo,
+            cms: cms,
+            important: important
+        };
+
+        $.ajax({
+           type: "POST",
+           url: wbf_ajax.ajaxurl,
+           data: {
+               action: 'check_data',
+               data: obj
+           },
+           success: (r) => {
+               console.log(r);
+           } ,
+           error: (e) => {
+               console.log(e);
+           }
+        });
+
+
+
+    });
+
+    // initalize datepicker
+    $('#finished_til').datepicker({
+        format: 'dd/mm/yyyy',
+        language: 'de-DE',
+        startDate: '01-01-2019'
+    }).on('changeDate', (e) => {
+        date = e.date;
+    });
+
+    const validateEmail = (e) => {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(e);
+    }
+
+});
--- wpbasicform.php
+++ wpbasicform.php
@@ -1,7 +1,88 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Marco
- * Date: 06.09.2018
- * Time: 14:26
- */
+Plugin Name: WP Basic Form
+Description: Basic WP Form
+Version: 0.1
+Author: mc17uulm
+AuthorURI: https://combosch.de
+License: GPLv3
+
+=== Plugin Information ===
+
+Version: 0.1
+Date: 06.09.2018
+
+*/
+
+if(!function_exists('add_action'))
+{
+    exit;
+}
+
+include_once 'app/App.php';
+
+define('WBF__PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+register_activation_hook(__FILE__, "wbf_activate");
+register_deactivation_hook(__FILE__, "wbf_deactivate");
+
+$dir = '/wp-content/plugins/WPBasicForm/';
+
+function wbf_activate()
+{
+
+
+}
+
+function wbf_deactivate()
+{
+
+
+}
+
+function wbf_enqueue_style()
+{
+
+    global $dir;
+
+    wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', array(), false, 'all');
+    wp_enqueue_style('bootstrap-datepicker', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css', array('bootstrap'), false, 'all');
+    wp_enqueue_style('wbf', $dir . 'css/custom.css', array('bootstrap'), false, 'all');
+
+}
+
+function wbf_enqueue_script()
+{
+
+    global $dir;
+
+    wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', array('jquery'), null, false);
+    wp_enqueue_script("bootstrap-datepicker", "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js", array('jquery'), null, false);
+    wp_enqueue_script("bootstrap-datepicker-de", $dir . "js/bootstrap-datepicker.de.js", array('bootstrap-datepicker'), null, false);
+    wp_enqueue_script("wbf", $dir . "js/wbf.js", array('jquery'), null, false);
+
+    wp_localize_script('wbf', 'wbf_ajax', array(
+       'ajaxurl' => admin_url('admin-ajax.php')
+    ));
+
+}
+
+add_action('wp_enqueue_scripts', 'wbf_enqueue_style');
+add_action('wp_enqueue_scripts', 'wbf_enqueue_script');
+
+function wbf_shortcode($atts)
+{
+
+    require('form.html');
+
+}
+
+add_shortcode("wbf", 'wbf_shortcode');
+
+add_action('wp_ajax_nopriv_check_data', 'wbf_handle_ajax_request');
+add_action('wp_ajax_check_data', 'wbf_handle_ajax_request');
+
+function wbf_handle_ajax_request()
+{
+    $_POST["data"];
+}
